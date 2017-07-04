4cb5ba36ef0adb0abe55d89db5d34e541f173e11
alfandersen <alf.andersen.1986@gmail.com>
2017-07-04 18:01:32 +0200

    initial commit

---
+++ app/src/main/java/alf/stream/radiostream/MainActivity.java
@@ -0,0 +1,86 @@
+package alf.stream.radiostream;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RadioStreamer radioStreamer;
+    private Spinner stationSpinner;
+    private ToggleButton playToggle;
+    private Context context;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        context = this.getApplicationContext();
+
+
+        // Radio Streamer
+        radioStreamer = new RadioStreamer(this);
+
+
+
+        // Select Radio Station Spinner
+        stationSpinner = (Spinner) findViewById(R.id.stationSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.radio_stations, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        stationSpinner.setAdapter(adapter);
+
+        stationSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                radioStreamer.setStation(i);
+                if(playToggle.isChecked()){
+                    ((TextView) stationSpinner.getChildAt(0)).setTextColor(Color.BLACK);
+                    stationSpinner.setBackgroundColor(ContextCompat.getColor(context, R.color.on));
+                }
+                else {
+                    ((TextView) stationSpinner.getChildAt(0)).setTextColor(Color.WHITE);
+                    stationSpinner.setBackgroundColor(ContextCompat.getColor(context, R.color.off));
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {}
+        });
+
+        // Play Radio Button
+        playToggle = (ToggleButton) findViewById(R.id.playToggle);
+        playToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b) {
+                    radioStreamer.start();
+                    ((TextView) stationSpinner.getChildAt(0)).setTextColor(Color.BLACK);
+                    stationSpinner.setBackgroundColor(ContextCompat.getColor(context, R.color.on));
+
+                }
+                else {
+                    radioStreamer.stop();
+                    ((TextView) stationSpinner.getChildAt(0)).setTextColor(Color.WHITE);
+                    stationSpinner.setBackgroundColor(ContextCompat.getColor(context, R.color.off));
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        radioStreamer.stop();
+    }
+}
---
+++ app/src/main/java/alf/stream/radiostream/RadioStreamer.java
@@ -0,0 +1,96 @@
+package alf.stream.radiostream;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.net.Uri;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static alf.stream.radiostream.RadioStreamer.RadioStation.NEWS;
+import static alf.stream.radiostream.RadioStreamer.RadioStation.P1;
+import static alf.stream.radiostream.RadioStreamer.RadioStation.P2;
+import static alf.stream.radiostream.RadioStreamer.RadioStation.P3;
+import static alf.stream.radiostream.RadioStreamer.RadioStation.P4;
+import static alf.stream.radiostream.RadioStreamer.RadioStation.P5;
+import static alf.stream.radiostream.RadioStreamer.RadioStation.P6;
+import static alf.stream.radiostream.RadioStreamer.RadioStation.P7;
+import static alf.stream.radiostream.RadioStreamer.RadioStation.P8;
+
+/**
+ * Created by Alf on 7/4/2017.
+ */
+
+public class RadioStreamer {
+
+    public enum RadioStation {
+        P1,P2,P3,P4,P5,P6,P7,P8,NEWS
+    }
+
+    private Activity parentActivity;
+    private MediaPlayer player;
+    private Map<RadioStation,String> stations;
+    RadioStation currentStation;
+
+
+    public RadioStreamer(Activity parentActivity){
+        this.parentActivity = parentActivity;
+//        setContentView(R.layout.activity_main);
+        String[] urls = parentActivity.getResources().getStringArray(R.array.streams);
+        stations = new HashMap<>();
+
+        stations.put(NEWS, urls[0]);
+        stations.put(P1, urls[1]);
+        stations.put(P2, urls[2]);
+        stations.put(P3, urls[3]);
+        stations.put(P4, urls[4]);
+        stations.put(P5, urls[5]);
+        stations.put(P6, urls[6]);
+        stations.put(P7, urls[7]);
+        stations.put(P8, urls[8]);
+
+        currentStation = P6;
+    }
+
+
+
+    private void updatePlayer(){
+        player = MediaPlayer.create(parentActivity, Uri.parse(stations.get(currentStation)));
+    }
+
+    public void start(){
+        updatePlayer();
+        player.start();
+    }
+
+    public void stop(){
+        if(player != null && player.isPlaying()) {
+            player.stop();
+        }
+    }
+
+    public void setStation(RadioStation station){
+        if(station != currentStation && stations.containsKey(station)) {
+            currentStation = station;
+            if (player != null && player.isPlaying()) {
+                stop();
+                updatePlayer();
+                start();
+            }
+        }
+    }
+
+    public void setStation(int stationNo){
+        switch (stationNo) {
+            case 0: setStation(NEWS); break;
+            case 1: setStation(P1); break;
+            case 2: setStation(P2); break;
+            case 3: setStation(P3); break;
+            case 4: setStation(P4); break;
+            case 5: setStation(P5); break;
+            case 6: setStation(P6); break;
+            case 7: setStation(P7); break;
+            case 8: setStation(P8); break;
+        }
+    }
+}
